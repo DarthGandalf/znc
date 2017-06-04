3b9d63e72c5eb9ea0b5607cc2eaa1270d4d1d3b3
Alexey Sokolov <alexey+znc@asokolov.org>
2017-06-04 20:49:12 +0100

    master_doc

--- doc/conf.py
+++ doc/conf.py
@@ -4,5 +4,8 @@ import os
 sys.path.insert(0, os.path.abspath('.'))
 extensions = ['doxy']
 
+master_doc = 'index'
 exclude_patterns = ['_build']
+
+# Just copy all doxygen files over
 html_extra_path = ['html']
